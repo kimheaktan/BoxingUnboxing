d70948139db48938e84f90476c58de0017e6e86b
Kimheaktan <54483773+kimheaktan@users.noreply.github.com>
2020-06-21 10:43:59 -0700

    first

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> things = new List<object>();
+            things.Add(7);
+            things.Add(28);
+            things.Add(-1);
+            things.Add("true");
+            things.Add("chair");
+
+
+            int sum = 0;
+            foreach (var item in things){
+                Console.WriteLine(item);
+                if(item is int){
+                    int NowAsString = (int)item;
+                    sum += NowAsString;
+                    //Or
+                    // sum += (int)item;
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
